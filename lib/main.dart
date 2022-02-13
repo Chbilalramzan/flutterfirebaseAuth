659db765cfde08b2939002cb9edcdcb67e46449c
Bilal Ramzan <chbilalramzan@gmail.com>
2022-02-13 23:58:44 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CircularProgressIndicator(),
+      home: const SignUp(),
     );
   }
 }
